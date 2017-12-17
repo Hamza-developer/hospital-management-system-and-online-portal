8592750ed0f089fa27cd854289138e44a0d3ffe4
unknown <JibranYousuf>
2017-12-17 14:26:12 +0500

    doc portal done

---
+++ server/models/authentication.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordConf: {
+    type: String,
+    required: true,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  }
+}, { collection: 'users' });
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
